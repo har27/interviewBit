ac95ca7a965217453e420405c10e63696a2cb066
har27 <51649694+har27@users.noreply.github.com>
2019-06-10 20:42:30 +0530

    Create MinimumCharactersrequiredtomakeaStringPalindromic.java

---
+++ Strings/MinimumCharactersrequiredtomakeaStringPalindromic.java
@@ -0,0 +1,30 @@
+/*
+You are given a string. The only operation allowed is to insert characters in the beginning of the string. How many minimum characters are needed to be inserted to make the string a palindrome string
+
+Example:
+Input: ABC
+Output: 2
+Input: AACECAAAA
+Output: 2
+*/
+public class Solution {
+    public String removechar(String str, int p) {  
+        return str.substring(0, p) + str.substring(p + 1);  
+    }  
+    public boolean ispalindromic(String a){
+        for(int i=0;i<a.length()/2;i++){
+            if(a.charAt(i)!=a.charAt(a.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int solve(String A) {
+        int count=0;
+        while(!ispalindromic(A)){
+            A=removechar(A,A.length()-1);
+            count++;
+        }
+        return count;
+    }
+}
